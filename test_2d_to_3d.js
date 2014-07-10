4fcee452b68aaa81973b3d4a01a5e557fbb3751c
Dan Taeyoung <provolot@gmail.com>
2014-07-10 17:18:37 -0400

    isolated line drawing to separate file

--- test_2d_to_3d.js
+++ test_2d_to_3d.js
@@ -13,172 +13,6 @@ var dotCitedFlag = true;
 var d3chart = d3chart || {};
 
 
-straightLine = [];
-
-function drawD3Chart() {
-
-	var svg;
-
-	var xScale;
-	var xAxis;
-	var years = [];
-	var uniqueYears;
-	var uniqueAuthors;
-	var uniqueKeywords;
-	var journalTypes = [];
-	var authors = [];
-	var keywords = [];
-	var goSecond = false;
-
-	var totals = [];
-	var totalAuthors = [];
-	var totalKeywords = [];
-
-	var total1 = 0;
-
-	var color =  d3.scale.category20c();
-	var opacityMap;
-
-	var firstLoadVar;
-	var firstLoad = -1;
-	var secLoad = -1;
-
-	var padding = 35;
-
-	var minYear;
-	var maxYear;
-	var maxAuthor;
-	var thisTotal;
-	var eachPaper;
-
-	var heightScale;
-	var singleScale;
-	var thisData = [];
-	var theseAuthors = [];
-	var theseKeywords = [];
-	var theX = [];
-	var maxEntries;
-	var width = 1400;
-	var height = 720;
-	var newX = [];
-	var newY = [];
-
-
-
-	d3.csv("memory_allyears_smallBatch.csv", function(data) {
-			thisData=(data);
-		for (i = 0;i<thisData.length; i++){ 
-			years[i] = data[i].Year;
-			authors[i] = data[i].Authors.split("., ");
-			for (j=0; j<authors[i].length; j++){
-			theseAuthors.push(authors[i][j]);            
-			}
-			journalTypes[i] = data[i].Sourcetitle;
-		}
-
-
-	////finds unique names etc
-		function onlyUnique(value, index, self) { 
-			return self.indexOf(value) === index;
-		} 
-		uniqueTypes = journalTypes.filter( onlyUnique ); //finds unique names
-		uniqueYears = years.filter( onlyUnique ); //finds unique names
-
-	////consolidates the Value for all values of a given Name
-		function valueConsolidation(givenYear, i) { 
-			var total = 0;
-			for (i = 0;i<data.length; i++){ 
-				if(data[i].Year== givenYear){
-					total++;
-				}else{
-				}}
-			 return total;
-		 } 
-	 //creates a new aray with the sums of all the different Names 
-		for (i = 0; i<uniqueYears.length; i++){
-			totals[i]= valueConsolidation(uniqueYears[i])
-		} 
-
-	   maxAuthor = d3.max(totalAuthors, function(d) { return d; });
-	   singleScale = d3.scale.linear()
-			.domain([1, maxAuthor*5])
-			.range([1, height/6-100]);
-
-		maxEntries = d3.max(totals, function(d) { return d; });
-
-		minYear = d3.min(years, function(d) { return d; });
-		maxYear = d3.max(years, function(d) { return d; });
-
-		d3chart.xScale = d3.scale.linear()
-			.domain([minYear, maxYear]) //not min year to max year
-			.range([100, width*1.2]);
-
-		var maxCited = d3.max(data, function(d) { return d.Cited; });
-		opacityMap = d3.scale.linear()
-			.domain([0, maxCited])
-			.range([.2, 1])        
-
-		heightScale = d3.scale.linear()
-			.domain([0, maxEntries*3])
-			.range([padding, height/1.2]);
-
-
-	for (i=0; i<thisData.length; i++){
-		newX.push(d3chart.xScale(years[i]))
-		newY.push(thisData[i].Cited) //height-10-
-	}
-
-	for (i=0; i<thisData.length; i++){
-		straightLine.push({
-			x: newX[i],
-			y: newY[i]
-		})
-	}
-
-
-	////FOR X AXIS
-	// var xTime = d3.time.scale()
-	//     .domain([new Date(1965,7,1), new Date(2014,7,1)])
-	//     .range([65, w-46]);
-
-	// xAxis = d3.svg.axis()
-	//     .scale(xTime)
-	//     .ticks(d3.time.years)
-	//     .tickSize(6,0)
-	//     .orient("bottom");
-
-	// svg.append("g")
-	//     .attr("class", "axis")  //Assign "axis" class
-	//     .attr("transform", "translate(0," + (h - padding+5) + ")")
-	//     .call(xAxis);
-
-
-    var geometryLine = new THREE.Geometry();
-    materialLine = new THREE.LineBasicMaterial({
-        color: 0x0000ff,
-    });
-
-    var originX = 0, originY = 0; 
-
-    for (var pt = 0; pt < straightLine.length; ++pt) {
-        var o = straightLine[pt];
-        console.log(straightLine[pt])
-        console.log(straightLine[pt].x)
-        geometryLine.vertices.push(new THREE.Vector3(originX+o.x, originY+o.y, Math.sin(pt/ 10.0) * 100));
-    }
-	console.log("GEOMLINE");
-	console.log(geometryLine.vertices);
-    line = new THREE.Line(geometryLine, materialLine);
-    scene.add(line);
-	console.log(line);
-
-
-
-	})
-
-}
-
-
 
 function threejs_d3_functions() {
 	/* THIS IS EVERYTHING TO KNIT D3 AND THREEJS TOGETHER */
@@ -289,15 +123,6 @@ function threejs_environment_init() {
 }
 
 
-function onWindowResize() {
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-
-	camera.aspect = width / height;				
-	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
-}
 
 function drawThreejsChart(csvFilename) {
 /* MAGIC IS HERE */
@@ -333,16 +158,7 @@ function drawThreejsChart(csvFilename) {
 				return newCircleSprite(parseInt("0x" + color(i).substr(1), 16));
 			});
 
-		// path = d3.select(line)
-		// 	.selectAll("THREE.Mesh")
-		// 	.data(data)
-		// 	.enter()
-		// 	.append(function(d, i) { 
-		// 		return newSphere(7057110);
-		// 	});	
-});
-
-
+	});
 
 }
 
@@ -356,39 +172,23 @@ function dotCited() {
 	.duration(3000)
 	.attr("position.x", function(d, i) { return d3chart.xScale(d['Year']); })
 	.attr("position.y", function(d, i) { return d['Cited'] ; })
-//		.attr("position.z", function(d, i) { return d['Cited'] * 1 ; })
 
 
-	// moreDots
-	// .transition()
-	// .duration(3000)
-	// .attr("position.x", function(d, i) { return d3chart.xScale(d['Year']); })
-	// .attr("position.y", function(d, i) { return d['Cited'] ; })
-
-
-	// rotationX = 0.01;
-	// rotationY = 0.01;
-	// rotationZ = 0.01;
+	 rotationX = 0.01;
+	 rotationY = 0.01;
+	 rotationZ = 0.01;
 }
 
 function dotPage() {
 	console.log("dotPage");
 
-	// moreDots
-	// .transition()
-	// .duration(3000)
-	// .attr("position.x", function(d, i) { return d3chart.xScale(d['Year']); })
-	// .attr("position.y", function(d, i) { return d['Cited'] ; })
 
 	moreDots
 	.transition()
 	.duration(3000)
 	.attr("position.x", function(d, i) { return d3chart.xScale(d['Year']); })
 	.attr("position.y", function(d, i) { return d['Cited'] ; })
-	// .attr("position.x", function(d, i) { return 30 * i; })
-	// .attr("position.z", function(d, i) { return d['Page end'] * 1 ; })
-		// .attr("position.z", function(d, i) { return d['Page end'] * 1 ; })
-		.attr("position.z", function(d, i) { return Math.sin(i/ 10.0) * 100 ; })
+	.attr("position.z", function(d, i) { return Math.sin(i/ 10.0) * 100 ; })
 
 	secondRotationX = 0.01;
 	secondRotationY = 0.01;
@@ -411,26 +211,29 @@ function threejs_animate() {
 	secondChart.rotation.y += secondRotationY;
 	secondChart.rotation.z += secondRotationZ; 
 
-	line.rotation.x += secondRotationX;
-	line.rotation.y += secondRotationY;
-	line.rotation.z += secondRotationZ;
+	animateLine();
 	renderer.render( scene, camera );
-//	renderSprites();
 
 }
 
+
+
+function onWindowResize() {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+
+	camera.aspect = width / height;				
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 
 function drawThings() {
-
-	// draw threejs chart
-	drawD3Chart();
-
+	drawLine(scene);
 	drawThreejsChart("memory_allyears_smallBatch.csv");
-
 }
 
-var b = 0;
 
 $( document ).ready(function() {
 
@@ -447,6 +250,7 @@ $( document ).ready(function() {
 	threejs_animate();  */
 
 
+	var b = 0;
 	$("body").keypress(function(){
 		console.log(b);
 		(b+=1);
@@ -459,17 +263,14 @@ $( document ).ready(function() {
 		if(b==3){
 			threejs_animate(); 
 		}
-	})	
+	})	 
+
 	$("#cited").click(function() {
 		console.log("cited clicked");
 		if(dotCitedFlag == true) {
 			dotCited();
-			// dotPage();
 			dotCitedFlag = false;
 		} else {
-	// rotationX = 0.0;
-	// rotationY = 0.0;
-	// rotationZ = 0.0;
 			dotPage();
 			dotCitedFlag = true;
 		}
